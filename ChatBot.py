2c3a25bc7211f2986f6fd80a489725048c97c3fb
dlm256 <kgcs256@gmail.com>
2025-01-21 13:54:37 +0000

    Tarefa
    Submitted task

---
+++ ChatBot.py
@@ -0,0 +1,13 @@
+while True:   
+    try:
+        nome = input("1ºNome: ")
+        print(f"Olá {nome}!")
+
+        idade = int(input("Idade: "))
+        print(f"Parabéns pelos seus {idade} anos! " )
+
+        apelido = input("Apelido: ")
+        print(f"Prazer, {nome} {apelido}!")
+        break
+    except ValueError:
+        print("Erro! Deve introduzir um numero!")
