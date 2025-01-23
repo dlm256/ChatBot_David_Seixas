fcc9e363db98ad8c88e3eef4fca898cdb25d71b6
dlm256 <kgcs256@gmail.com>
2025-01-23 14:47:04 +0000

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -1,13 +1,17 @@
-while True:   
-    try:
-        nome = input("1ºNome: ")
-        print(f"Olá {nome}!")
+def Greet():    
+    while True:   
+        try:
+            nome = input("1ºNome: ")
+            print(f"Olá {nome}!")
 
-        idade = int(input("Idade: "))
-        print(f"Parabéns pelos seus {idade} anos! " )
+            idade = int(input("Idade: "))
+            print(f"Parabéns pelos seus {idade} anos! " )
 
-        apelido = input("Apelido: ")
-        print(f"Prazer, {nome} {apelido}!")
-        break
-    except ValueError:
-        print("Erro! Deve introduzir um numero!")
+            apelido = input("Apelido: ")
+            print(f"Prazer, {nome} {apelido}!")
+            break
+        except ValueError:
+            print("Erro! Deve introduzir um numero!")
+            
+            
+
